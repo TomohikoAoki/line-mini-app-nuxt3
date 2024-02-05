43ef8c540386cd325120345ac0bf71b8cd0cf4af
TomohikoAoki <isaoaokiaaa@gmail.com>
2024-02-05 12:50:05 +0900

    まずやってみる

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>{{ sdkVersion }}</p>
+        <p>{{ token }}</p>
+        <p>{{ profileData }}</p>
+    </div>
+</template>
+
+<script setup>
+const sdkVersion = ref('')
+const token = ref('')
+const profileData = ref('')
+
+
+onMounted(() => {
+    const { $liffInit, liff } = useNuxtApp()
+
+
+    $liffInit
+        .then(async () => {
+            sdkVersion.value = liff.getVersion();
+
+            token.value = await liff.getIDToken();
+            profileData.value = await liff.getDecodedIDToken()
+
+            // //storeにLINEのtokenとprofileを保存
+            // this.$store.dispatch('setToken', token)
+            // this.$store.dispatch('setProfile', profileData)
+
+            // this.$router.push('/top')
+        })
+        .catch((error) => {
+            console.error(error);
+            // this.showButton = true;
+        });
+
+})
+</script>
---
+++ plugins/liff-init.client.js
@@ -0,0 +1,26 @@
+
+import liff from '@line/liff';
+
+
+export default defineNuxtPlugin(nuxtApp => {
+  const runtimeConfig = useRuntimeConfig()
+
+
+  const initResult = liff.init({ liffId: '2001061637-2gNwm9z5', withLoginOnExternalBrowser: true })
+    .then(() => {
+      console.log('liff.init() done');
+    })
+    .catch(error => {
+      console.log(`liff.init() failed: ${error}`);
+      if (!process.env.liffId) {
+        console.info('LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.');
+      }
+      return Promise.reject(error);
+    });
+  return {
+    provide: {
+      liff: liff,
+      liffInit: initResult
+    }
+  }
+})
