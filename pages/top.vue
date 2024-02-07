a71b666e1ae815feb07e4316da8be8c8e4e56614
TomohikoAoki <isaoaokiaaa@gmail.com>
2024-02-07 14:49:22 +0900

    わけわかんね

--- pages/top.vue
+++ pages/top.vue
@@ -92,7 +92,7 @@ const connectMemberByLineToken = async () => {
     if (!error.value) {
         response.value = data
         message.value = '会員情報との紐づけができました。'
-        userState.value.point = data.data.point === undefined ? 100 : data.data.point
+        userState.value.point = 100
         loading.value = false
 
         return
