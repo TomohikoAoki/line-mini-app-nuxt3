947fd721c4c1505d55ac948426b8cdea8bbef2d9
TomohikoAoki <isaoaokiaaa@gmail.com>
2024-02-08 20:43:26 +0900

    なんとかできた？

--- plugins/body-scroll-lock.ts
+++ plugins/body-scroll-lock.ts
@@ -1,11 +1,11 @@
 declare module 'body-scroll-lock' {
     interface bodyScrollOptions {
-        reserveScrollBarGap?: boolean;
-        allowTouchMove?: (el: HTMLElement) => boolean;
+        reserveScrollBarGap?: boolean | undefined;
+        allowTouchMove?: ((el: HTMLElement | Element) => boolean) | undefined;
     }
 
-    export function disableBodyScroll(targetElement: HTMLElement, options?: bodyScrollOptions): void;
-    export function enableBodyScroll(targetElement: HTMLElement): void;
+    export function disableBodyScroll(targetElement: HTMLElement | Element, options?: bodyScrollOptions): void;
+    export function enableBodyScroll(targetElement: HTMLElement | Element): void;
     export function clearAllBodyScrollLocks(): void;
 
 }
