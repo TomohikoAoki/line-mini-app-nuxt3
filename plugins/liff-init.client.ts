ec3fca6b2e2ca82a7cb9bcedf2f0c5174d08708b
TomohikoAoki <isaoaokiaaa@gmail.com>
2024-02-09 14:41:39 +0900

    jてすとー

--- plugins/liff-init.client.ts
+++ plugins/liff-init.client.ts
@@ -8,24 +8,24 @@
 
 // import liff from '@line/liff';
 
-// export default defineNuxtPlugin(nuxtApp => {
-//     const runtimeConfig = useRuntimeConfig()
+export default defineNuxtPlugin(nuxtApp => {
+    const runtimeConfig = useRuntimeConfig()
 
-//     const initResult: Promise<string | void> = liff.init({ liffId: `${runtimeConfig.public.liffId}`, withLoginOnExternalBrowser: true })
-//         .then(() => {
-//             console.log('liff.init() done');
-//         })
-//         .catch(error => {
-//             console.log(`liff.init() failed: ${error}`);
-//             if (!process.env.liffId) {
-//                 console.info('LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.');
-//             }
-//             return Promise.reject(error);
-//         });
-//     return {
-//         provide: {
-//             liff: liff,
-//             liffInit: initResult
-//         }
-//     }
-// })
+    //     const initResult: Promise<string | void> = liff.init({ liffId: `${runtimeConfig.public.liffId}`, withLoginOnExternalBrowser: true })
+    //         .then(() => {
+    //             console.log('liff.init() done');
+    //         })
+    //         .catch(error => {
+    //             console.log(`liff.init() failed: ${error}`);
+    //             if (!process.env.liffId) {
+    //                 console.info('LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.');
+    //             }
+    //             return Promise.reject(error);
+    //         });
+    //     return {
+    //         provide: {
+    //             liff: liff,
+    //             liffInit: initResult
+    //         }
+    //     }
+})
